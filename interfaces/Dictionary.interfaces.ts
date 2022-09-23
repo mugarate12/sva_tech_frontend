b034e805be367be3489803b20ad66d7e7db6d1bf
Mateus Cardoso <serjumano17@gmail.com>
2022-09-23 02:34:08 -0300

    :sparkles: feat: add commun interface directory and Dictionary Interfaces

---
+++ interfaces/Dictionary.interfaces.ts
@@ -0,0 +1,27 @@
+export interface Word {
+  word: string;
+  phonetic: string;
+  phonetics: Array<{
+    text: string,
+    audio?: string
+  }>;
+  origin?: string;
+  meanings: Array<{
+    partOfSpeech: string,
+    definitions: Array<{
+      definition: string,
+      synonyms: Array<string>,
+      antonyms: Array<string>,
+      example?: string
+    }>,
+    synonyms: Array<string>;
+    antonyms: Array<string>;
+  }>;
+  license: {
+    name: string,
+    url: string
+  };
+  sourceUrls: Array<string>;
+};
+
+export type WordData = Array<Word>;
